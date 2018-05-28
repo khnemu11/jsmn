fe8c55fb4f84f864689a76184f8f568abf8d8980
khnemu11 <khnemu@naver.com>
2018-05-28 16:55:47 +0900

    solve example 7

--- resource/filesimple.c
+++ resource/filesimple.c
@@ -36,6 +36,29 @@ int count;
   }
 }
 
+int* printObjectNameList(char*jsonstr,jsmntok_t *t,int tokcount,int* objectNameIndex){
+  int i;
+  int j;
+
+  printf("*******Object List******\n");
+
+  for(i = 1,j =0; i < tokcount; i++){
+      if(t[i].size == 8){
+        printf("name[%2d] : %.*s\n",j+1,t[i+2].end-t[i+2].start,jsonstr + t[i+2].start);
+        if(j==0){
+          objectNameIndex=(int*)malloc(sizeof(int));
+          objectNameIndex[j]=i;
+        }
+        else{
+          objectNameIndex=(int*)realloc(objectNameIndex,sizeof(objectNameIndex)+sizeof(int));
+          objectNameIndex[j]=i;
+        }
+        j++;
+      }
+  }
+  return objectNameIndex;
+}
+
 void printNameList(char*jsonstr,jsmntok_t *t, int *nameTokIndex){
  int i=0;
 
@@ -72,6 +95,30 @@ void selectNameList(char *jsonstr, jsmntok_t *t, int *nameTokIndex){
   }
 }
 
+void selectObjectNameList(char *jsonstr, jsmntok_t *t, int *objectNameIndex){
+  int index;
+  int j;
+
+  while(1){
+    printf("Select Name's no (exit:0) >> ");
+    scanf("%d",&index);
+    
+    if(index==0){
+      printf("exit");
+      break;
+    }
+
+    else if(index-1<=sizeof(objectNameIndex)/sizeof(int)){
+      printf("name[%2d] : %.*s\n",index,t[objectNameIndex[index-1]+2].end-t[objectNameIndex[index-1]+2].start,jsonstr + t[objectNameIndex[index-1]+2].start);
+      printf("%.*s\n",t[objectNameIndex[index-1]].end-t[objectNameIndex[index-1]].start,jsonstr + t[objectNameIndex[index-1]].start);
+    }
+    else{
+      printf("input right index\n");
+    }
+  }
+}
+
+
 static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
 	if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
 			strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
@@ -86,6 +133,7 @@ int main() {
 	int nameTokIndex[100]={0,};
 	jsmn_parser p;
 	jsmntok_t t[128]; /* We expect no more than 128 tokens */
+	int *objectNameIndex;	
 
   	JSON_STRING=readJSONFile();
   
@@ -108,8 +156,10 @@ int main() {
 	}
 
 	jsonNameList(JSON_STRING,t,r,nameTokIndex);	
-	printNameList(JSON_STRING,t,nameTokIndex);
-	selectNameList(JSON_STRING,t, nameTokIndex);
-	
+	objectNameIndex=printObjectNameList(JSON_STRING,t,r,objectNameIndex);
+	//printNameList(JSON_STRING,t,nameTokIndex);
+	//selectNameList(JSON_STRING,t, nameTokIndex);
+	selectObjectNameList(JSON_STRING,t,objectNameIndex);	
+
 	return EXIT_SUCCESS;
 }
